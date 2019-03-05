c7ba9dd2e4205c5403bb75ac620280a3eb8c1596
q55mehqs <eviyuyu14@gmail.com>
2019-03-05 23:25:56 +0900

    ツイートをすべての関数で判定にかけたときの判定をする関数の作成

--- twitter_option.py
+++ twitter_option.py
@@ -119,6 +119,33 @@ def isForeignLanguage(tweet : dict) -> bool:
         print("引数エラー ツイートがありません")
 
 
+def isAllCheck_or(tweet, id_str="", screen_name="") -> bool:
+    """入れたツイートをすべての関数でチェックして
+    すべてorでかけたものを返します
+
+    Parameter
+    ---------
+    tweet : dict
+        (必須)確認するツイートの情報が入ったdict型を入れてください
+
+    id_str : str
+        (screen_nameかどちらか必須)ユーザーidの数字列を文字列型にして
+        入れてください。どちらも入ってればこちらが優先されます
+    
+    screen_name : str
+        (id_strかどちらか必須)スクリーンネーム(@Q55mEhQS の Q55mEhQS)
+        を入れてください
+    
+    Return
+    ------
+    bool
+        ここにある関数をすべてorでつないだものを返します
+    """
+
+    return (isProtect(tweet) or isOwner(tweet, id_str=id_str, screen_name=screen_name) or 
+            isReply(tweet) or isForeignLanguage(tweet))
+
+
 if __name__ == "__main__":
     from twitters import user_timeline
     tweet = user_timeline(screen_name="Q55mEhQS", count=10, no_catch_reply=False)[0]
@@ -128,3 +155,4 @@ if __name__ == "__main__":
     print("Owner: %s" % str(isOwner(tweet, screen_name="Q55mEhQS")))
     print("Reply: %s" % str(isReply(tweet)))
     print("Lang!=ja: %s" % str(isForeignLanguage(tweet)))
+    print("all: %s" % str(isAllCheck_or(tweet, screen_name="Q55mEhQS")))
